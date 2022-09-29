9ff9ba10e0b44b76530ebbbd5b2d9e2f5588a598
luijaro <luis.rojase@usach.cl>
2022-09-28 21:06:39 -0300

    Update main.js

--- main.js
+++ main.js
@@ -42,8 +42,8 @@ client.on('message', message => {
 
         const embed = new Discord.MessageEmbed()
             .setTitle('Mensaje de')
-             embed.addField(user)
-             embed.addField(result)
+             embed.addField(user,'\n')
+             embed.addField(result,'\n')
             
         message.delete();
 
